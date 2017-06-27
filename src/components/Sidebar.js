ec3869015b2bf63ff14dd51ff7497922069ae91e
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-27 14:50:32 -0700

    Categorize sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import GBox from 'grommet/components/Box';
 import GImage from 'grommet/components/Image';
 import GMenu from 'grommet/components/Menu';
 import GAnchor from 'grommet/components/Anchor';
+import GHeading from 'grommet/components/Heading';
 
 import SearchIcon from 'grommet/components/icons/base/Search';
 import UserIcon from 'grommet/components/icons/base/User';
@@ -12,15 +13,33 @@ import logo from '../../assets/images/logo.png';
 
 const Header = () => (
   <GBox>
-    <GBox size="small" pad="medium">
-      <GAnchor path="/">
-        <GImage src={logo} fit="cover" />
-      </GAnchor>
-    </GBox>
     <GMenu inline={true} direction="column" primary={true} size="small">
+      <GBox size="small" pad="medium" style={{ paddingBottom: '0px' }}>
+        <GAnchor path="/">
+          <GImage style={{ width: '80px' }} src={logo} />
+        </GAnchor>
+      </GBox>
+      <GBox pad="medium" style={{ paddingBottom: '0px' }}>
+        <GHeading tag="h4">
+          Onboarding
+        </GHeading>
+      </GBox>
       <GAnchor label="Create Marketplace" path="/create-marketplace" />
       <GAnchor label="Create Admin" path="/create-admin" />
-      <GAnchor label="Mark as old" path="/create-admin" />
+      <GBox pad="medium" style={{ paddingBottom: '0px' }}>
+        <GHeading tag="h4">
+          Items
+        </GHeading>
+      </GBox>
+      <GAnchor label="Edit Item" path="/edit-item" />
+      <GAnchor label="Refund Item" path="/refund-item" />
+      <GBox pad="medium" style={{ paddingBottom: '0px' }}>
+        <GHeading tag="h4">
+          Misc
+        </GHeading>
+      </GBox>
+      <GAnchor label="List Callbacks" path="/list-callbacks" />
+      <GAnchor label="Move Funds" path="/move-funds" />
     </GMenu>
   </GBox>
 );
