f6e319e8de897c1bfb2a5e8f4bed4532950cef47
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-28 15:35:00 -0700

    Add redux and initial reducer

--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 import GBox from 'grommet/components/Box';
 
-const Credentials = () => (
-  <GBox colorIndex="light-2" size="large" pad="small" margin={{ vertical: 'medium' }}>
-    <h3>Credentials</h3>
-    <div>Marketplace: test.com</div>
-    <div>Username: admin@test.com</div>
-  </GBox>
-);
+const Credentials = (user) => {
+  const Info = () => (
+    <div>
+      <h3>Credentials</h3>
+      <div>Marketplace: { user.marketplace }</div>
+      <div>Username: { user.username }</div>
+    </div>
+  );
+
+  return (
+    <GBox colorIndex="light-2" size="large" pad="small" margin={{ vertical: 'medium' }}>
+      { user.username ? <Info /> : <h3>Please log in</h3> }
+    </GBox>
+  );
+}
 
 export default Credentials;
